f325d0b5f6f5a674b416c28c4e0b4a136890b83c
Muhammed Karabulut <muhammedkarabulut3645@gmail.com>
2026-02-26 22:11:08 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+return (
+ <div>
+ <h1>Web Tasarimi ve Programlama </h1>
+ <h2>LAB -1</h2>
+ <p>Ad Soyad: [MUHAMMED SIDDIK KARABULUT]</p>
+ <p>Ogrenci No: [230541134]</p>
+ </div>
+ )
+ }
+
+ export default App
